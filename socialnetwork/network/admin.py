ed881224d4857b9556242b8428422545837b6570
Reshetnyak.S <kbaep1953@gmail.com>
2022-06-02 11:42:48 +0300

    delete comment django

--- socialnetwork/network/admin.py
+++ socialnetwork/network/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import User
 from .models import Post
-# Register your models here.
 
 admin.site.register(Post)
--- socialnetwork/network/models.py
+++ socialnetwork/network/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
